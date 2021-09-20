23b15c2b71ae63ceca7f93abd4b9a5ea7ad46893
trisvafeles33 <90238122+trisvafeles33@users.noreply.github.com>
2021-09-20 05:51:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+insect = "Bee"
+
+print(insect)
+print(insect[0:2])
+print(insect[0], end = "\n\n")
+
+print(insect)
+print(insect, insect)
+print(insect, end = " ")
+print(insect, insect, end = "\n\n")
+
+print(insect.upper())
+print(insect.lower())
