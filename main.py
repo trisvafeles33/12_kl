0fbf402dca83432501337f5ad3f7f16f98469767
trisvafeles33 <90238122+trisvafeles33@users.noreply.github.com>
2021-09-20 06:31:15 +0000

    add format

--- main.py
+++ main.py
@@ -10,4 +10,19 @@ print(insect, end = " ")
 print(insect, insect, end = "\n\n")
 
 print(insect.upper())
-print(insect.lower())
+print(insect.lower(), end = "\n\n")
+
+print(insect[3::-1])
+print(insect, end = "\n\n")
+
+tree1 = "egle"
+tree2 = "bērzs"
+#string
+print(f"Ārā pie celtnes aug {tree1} un {tree2}.")
+#teksta un mainīgā savienošana
+print("Ārā pie celtnes aug " + tree1 + " un " + tree2 + ".")
+#.format
+print("Ārā pie celtnes aug {0} un {1}." .format("egle", "bērzs"))
+#%s
+print("Ārā pie celtnes aug %s" %tree1, end = " ")
+print("un %s." %tree2)
