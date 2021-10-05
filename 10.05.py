3cb05e5a4d321e88c713043b8946c4f5efccd35e
trisvafeles33 <90238122+trisvafeles33@users.noreply.github.com>
2021-10-05 06:22:27 +0000

    add 10.05.

---
+++ 10.05.py
@@ -0,0 +1,10 @@
+#1 kg
+āboli = 1.50
+cukurs = 0.80
+garšvielas = 3.50
+
+#Ja receptē ir nepieciešams 3 kg ābolu, 2 kg cukura un 1 kg garšvielu
+recepte = [3,2,1]
+for daudzums in range(0, len(recepte)):
+    summa = 0 + recepte[daudzums]
+print(summa)
