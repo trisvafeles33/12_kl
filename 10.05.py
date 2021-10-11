86fe017f8006e5a7f6ead52c537c5616dda08daa
trisvafeles33 <90238122+trisvafeles33@users.noreply.github.com>
2021-10-11 07:13:35 +0000

    add 10.11.

--- 10.05.py
+++ 10.05.py
@@ -1,3 +1,6 @@
+'''
+Ja ir jau dota cena
+'''
 #1 kg
 āboli = 1.50
 cukurs = 0.80
@@ -11,3 +14,47 @@ print("Ievārījuma receptē ir nepiecišami", sum1, "ingredienti.")
 #Produktu summa (cena)
 cena = 1.50+1.50+1.50+0.80+0.80+3.50
 print("Ingredienti maksās", cena, "eiro.")
+
+
+
+'''
+Ja cenu vajag ievadīt pašam
+'''
+a = int(input("Ābolu skaits kg: "))
+c = int(input("Cukura skaits kg: "))
+g = int(input("Garšvielu skaits kg: "))
+sum1 = a+c+g
+print("Receptē būs nepieciešams", sum1, "kg produktu.", end = "\n\n")
+
+a_c = float(input("Ābolu cena: "))
+c_c = float(input("Cukura cena: "))
+g_c = float(input("Garšvielu cena: "))
+sum2 = a_c*a+c_c*c+g_c*g
+print("Ievārījums maksās:", sum2, "eiro.")
+
+
+
+'''
+Skolotājas metode
+'''
+#Izmantot bibliotēkas
+recepte1 = {"cukurs":2.65, "kanēlis":0.43, "āboli":5.50, "ūdens":5.40}
+cenas1 = {"cukurs":1.50, "kanēlis":2.15, "āboli":3.00, "ūdens":0.40}
+
+def izmaksas_receptei(recepte,cenas):
+
+  summa=0
+  for sastāvdaļa in recepte:
+    daudzums = recepte[sastāvdaļa]
+  summa = daudzums*cenas[sastāvdaļa]
+  return summa
+
+def izmaksas_receptei(abolu_svars, recepte, cena):
+  izmaksas_kg=izmaksas_receptei(recepte, cenas)
+  recepte['āboli']
+
+  ievarijuma_izmaksas = abolu_svars + izmaksas_kg
+
+  return ievarijuma_izmaksas
+
+  print(izmaksas_kopa(15.0, recepte1, cenas1))
