1bc6620ad3efdc952e9e558d1e37f31ebcece291
trisvafeles33 <90238122+trisvafeles33@users.noreply.github.com>
2021-10-12 05:54:05 +0000

    add 10.12.

---
+++ main.py
@@ -0,0 +1,13 @@
+'''
+Uzrakstīt funkciju, kura atgriež vērtību true, ja abi dotie vārdi sākas ar vienu burtu
+'''
+
+sakas_vienadi("Liela lama") > True
+sakas_vienadi("Maza lama") > False
+
+'''
+Noderīgas metodes
+
+.split()
+.lower()
+'''
